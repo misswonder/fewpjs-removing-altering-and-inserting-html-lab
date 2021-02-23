7f2b0df2cd1c11312404209623c0a6af79bd5e5d
Yvonne Chen <yvonnechen1991@gmail.com>
2021-02-22 23:55:03 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const testVar = "hi";
+
+/* Enter the code to remove the node element here */
+document.querySelector("main#main").remove();
+const newHeader = document.createElement("h1");
+newHeader.className = "victory";
+newHeader.id = "victory";
+newHeader.innerHTML = "Flatiron School is the champion!";
